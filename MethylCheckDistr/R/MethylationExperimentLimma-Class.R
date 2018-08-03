7b8e1d3e76efb26a5b60f8974aa80fa8b7e55db5
Cristobal Fresno <cristobalfresno@gmail.com>
2018-08-03 09:24:42 -0500

    Added MethylationExperimentLimma class

---
+++ MethylCheckDistr/R/MethylationExperimentLimma-Class.R
@@ -0,0 +1,17 @@
+#' MethylationExperimentLimma-Class
+#'
+#' @description  This is a concrete class extends MethylationExperiment to impose a normal
+#'    distribution using limma package to fit it. In this context, as it
+#'    inherits from a SummarizedExperiment the fitted models will be included
+#'    a "model" column, in the features (rowData), where each model is stored
+#'    as a list.
+#' @docType class
+#' @exportClass MethylationExperimentLimma
+setClass(
+    Class = "MethylationExperimentLimma",
+    contains = "MethylationExperiment"
+)
+
+
+
+
